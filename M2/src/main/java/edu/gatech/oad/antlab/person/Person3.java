e4132f53ed7dc00c12bcf828884b4d2f6642b2d2
smada2017 <saivardhanmada@gmail.com>
2018-01-27 23:23:35 -0500

    changed person 3

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -4,7 +4,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  reversed string 
  *  
- *  @author  Bob
+ *  @author  Saivardhan Mada
  *  @version 1.1
  */
 public class Person3 {
@@ -43,6 +43,7 @@ public class Person3 {
 	 */
 	private String calc(String input) {
 	  //Person 3 put your implementation here
-	  return null;
+
+	  return new StringBuilder(input).reverse().toString();
 	}
 }
